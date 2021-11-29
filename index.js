ef23f7511b4ef794a0bdb55c43cf6f065c660976
SandhiyaSundaram <86886100+SandhiyaSundaram@users.noreply.github.com>
2021-11-29 11:51:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ToDo from './Component/ToDo';
+
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.render(
+
+  <div>
+    < ToDo></ToDo>
+  </div>,  
+  
+  
+  document.getElementById('root')
+);
